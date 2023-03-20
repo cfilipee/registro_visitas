dcfcde19a5428e0c09bab69bb6e06e668a8ff1a5
cfilipee <caiofilipee@gmail.com>
2023-03-20 16:03:18 -0300

    Delete main.js

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-
-// document.getElementById("btn-cadastrar").addEventListener("click", function(event){
-//   event.preventDefault();
-//   if (nome.value.length < 5) {
-//         nome.classList.add("errorInput")
-//     } else {
-//         nome.classList.remove("errorInput")
-//     }
-// });
