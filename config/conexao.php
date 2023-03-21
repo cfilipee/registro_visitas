e105bafc17717bb9cf899af1bcc085a37c2c0250
Caio <caiofilipee@gmail.com>
2023-03-21 12:41:35 -0300

    configurando arquivo de conexão banco de dados Umbler

--- config/conexao.php
+++ config/conexao.php
@@ -1,8 +1,8 @@
 <?php
-	$servidor = "localhost";
-	$usuario = "root";
-	$senha = "";
-	$dbname = "cadastro_teste";
+	$servidor = "mysql744.umbler.com:41890";
+	$usuario = "root_caio";
+	$senha = "8143Caio";
+	$dbname = "visitas_gabinete";
 	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 	if(!$conn){
 		echo "ERROR: 1";
