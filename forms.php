48574719b986ae447d3ace673921bf7334476353
Caio <caiofilipee@gmail.com>
2023-03-22 18:29:40 -0300

    criação arquivo formulario aberto

---
+++ forms.php
@@ -0,0 +1,132 @@
+<div class="modal fade" id="cadCliente" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header ">
+                <h5 class="modal-title" id="exampleModalLabel">CADASTRO DE CLIENTES</h5>
+
+
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+
+
+            </div>
+
+
+            <!-- ALERTA PARA ERRO DE PREENCHIMENTO DE FORMULARIO -->
+            <div class="alert alert-danger d-none fade show m-3" role="alert">
+                <strong>ERRO!</strong> - <strong>Preencha o campo <span id="campo-erro"></span></strong>!
+                <span id="msg"></span>
+            </div>
+
+            <div class="modal-body">
+                <form method="POST" id="insert_form">
+
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12">
+                            <label for="recipient-nome" class="col-form-label">Nome</label>
+                            <input type="text" name="nome" id="nome" maxlength="50" class="form-control">
+                        </div>
+                        <div class="col-md-6 col-sm-12">
+                            <label for="recipient-email" class="col-form-label">E-mail</label>
+                            <input type="email" name="email" id="email" maxlength="50" class="form-control -10">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-10 col-sm-12">
+                            <label for="recipient-rua" class="col-form-label">Rua</label>
+                            <input type="text" name="rua" id="rua" maxlength="50" class="form-control">
+                        </div>
+                        <div class="col-md-2 col-sm-12">
+                            <label for="recipient-numero" class="col-form-label">Nº</label>
+                            <input type="text" name="numero" id="numero" maxlength="50" class="form-control -10">
+                        </div>
+
+                    </div>
+                    <div class="row">
+                        <div class="col-md-5 col-sm-12">
+                            <label for="recipient-bairro" class="col-form-label">Bairro</label>
+                            <input type="text" name="bairro" id="bairro" maxlength="50" class="form-control">
+                        </div>
+                        <div class="col-md-5 col-sm-12">
+                            <label for="recipient-complemento" class="col-form-label">Complemento</label>
+                            <input type="text" name="complemento" id="complemento" maxlength="50" class="form-control -10">
+                        </div>
+                        <div class="col-md-2 col-sm-12">
+                            <label for="recipient-cep" class="col-form-label">Cep</label>
+                            <input type="text" name="cep" id="cep" maxlength="50" class="form-control -10 border border-warning" onblur="pesquisacep(this.value);">
+                        </div>
+
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4 col-sm-12">
+                            <label for="recipient-cidade" class="col-form-label">Cidade</label>
+                            <input type="text" name="cidade" id="cidade" maxlength="50" class="form-control">
+                        </div>
+                        <div class="col-md-2 col-sm-12">
+                            <label for="recipient-uf" class="col-form-label">UF</label>
+                            <input type="text" name="uf" id="uf" maxlength="50" class="form-control -10">
+                        </div>
+                        <div class="col-md-3 col-sm-12">
+                            <label for="recipient-telefone" class="col-form-label">Telefone</label>
+                            <input type="text" name="telefone" id="telefone" onkeypress="mask(this, telefone);" onblur="mask(this, telefone);" class="form-control -10">
+                        </div>
+                        <div class="col-md-3 col-sm-12">
+                            <label for="recipient-assunto" class="col-form-label">Assunto</label>
+                            <input type="text" name="assunto" id="assunto" maxlength="50" class="form-control -10">
+                        </div>
+
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4 col-sm-12">
+                            <label for="recipient-cargo" class="col-form-label">Cargo</label>
+                            <input type="text" name="cargo" id="cargo" maxlength="50" class="form-control">
+                        </div>
+                        <div class="col-md-4 col-sm-12">
+                            <label for="recipient-partido" class="col-form-label">Partido</label>
+                            <input type="text" name="partido" id="partido" maxlength="50" class="form-control -10">
+                        </div>
+                        <div class="col-md-4 col-sm-12">
+                            <label for="recipient-nascimento" class="col-form-label">Nascimento</label>
+                            <input type="text" name="nascimento" id="nascimento" class="form-control -10">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4 col-sm-12">
+                            <label for="recipient-operador" class="col-form-label cli">Operador</label>
+                            <input type="text" name="operador" id="operador" maxlength="50" class="form-control" disabled value="<?php echo $_SESSION['usuarioNome'] ?>">
+                        </div>
+                        <div class="col-md-4 col-sm-12">
+                            <label for="recipient-dataCadastro" class="col-form-label">Data do cadastro</label>
+                            <input type="text" class="form-control" value="<?php echo date('d/m/Y - H:i:s') ?>" disabled>
+                        </div>
+                        <div class="col-md-4 col-sm-12">
+
+                        <label for="recipient-situacao" class="col-form-label">Situação</label>
+                        <select class="form-control form-select-lg mb-5 select2" name="situacao" id="situacao" aria-label=".form-select-lg example">
+                            <option value="Pendente">Pendente</option>
+                            <option value="Ativo">Ativo</option>
+                            <option value="Inativo">Inativo</option>
+                            <option value="Cancelado">Cancelado</option>
+
+                        </select>
+                       
+                        </div>
+                    </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                <button type="submit" class="btn btn-primary" id="btn-cadastrar">Salvar</button>
+            </div>
+
+            </form>
+
+
+        </div>
+        <div class="m-2 col-md-8 col-sm-8 alert alert-primary ">
+            Informe o CEP e tecle [ ENTER ] para autopreencher o endereço !
+        </div>
+    </div>
+</div>
