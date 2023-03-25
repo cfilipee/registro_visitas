c21b3d90ca46ab0ef55f946b4442dfa65faa998d
Caio <caiofilipee@gmail.com>
2023-03-25 11:26:26 -0300

    criação do arquivo forms-obrigado.php e arquvo styles.css dentro do diretorio assets/css

---
+++ forms-obrigado.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Obrigado</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+
+<div id="main-container">
+    
+    <div id="obrigado-container">
+        <h2><ion-icon name="checkmark-outline"></ion-icon></h2>
+        <p>Cadastro Realizado com sucesso!</p>
+    </div>
+
+</div>
+
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
