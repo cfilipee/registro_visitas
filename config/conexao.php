778391992d2dd49287206996a6962b594fd6a6b3
Caio <caiofilipee@gmail.com>
2023-06-16 15:13:46 -0300

    re-configuração das informações do banco de dados

--- config/conexao.php
+++ config/conexao.php
@@ -1,11 +1,11 @@
 <?php
-	$servidor = "mysql744.umbler.com:41890";
-	$usuario = "root_caio";
-	$senha = "8143Caio";
-	$dbname = "visitas_gabinete";
-	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
-	if(!$conn){
-		echo "ERROR: 1";
-	}else{
-			//echo "Conexao realizada com sucesso";
-	}
+$servidor = "mysql744.umbler.com:41890";
+$usuario = "filipe_root";
+$senha = "8143filipe";
+$dbname = "visitas_gabinete";
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+if (!$conn) {
+	echo "ERROR: 1";
+} else {
+	//echo "Conexao realizada com sucesso";
+}
